071b10ccee4d387aa03bd8dc8c34ae6ed14917e3
youssefizikitn <husa2006.15@gmail.com>
2016-07-25 13:38:33 +0100

    Added some views

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/create', function (req, res, req) {
+	var vm = {
+		title: 'Create an account'
+	};
+	res.render('users/create');
+})
+
 module.exports = router;
